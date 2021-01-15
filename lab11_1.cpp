267138e24b459c2c89b53dfb8a074cb6e6ae966b
ford <tewarad2020>
2021-01-15 15:54:56 +0700

    code completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
+
 using namespace std;
+
+int fibonacci(int);
 
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+int fibonacci(int x) {
+    int sum = 0;
+    if (x == 0) sum = 0;
+    if (x == 1) sum = 1;
+    if (x > 1) sum += fibonacci(x-1) + fibonacci(x-2);
+    return sum;
 }
